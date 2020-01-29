feb7aadc2742c49d153fcd8265ab043913a626f4
Jud Taylor <jud.taylor@protonmail.com>
2020-01-29 18:47:45 -0500

    Trying to keep up!

--- twitter.py
+++ twitter.py
@@ -11,3 +11,28 @@ from .models import DB, Tweet, User
 # TWITTER = tweepy.API(TWITTER_AUTH)
 
 # BASILICA = basilica.Connectin(CONFIG('BASILIC_KEY
+
+def add_or_update_user()
+"""
+Add or update a user and their tweets..
+"""
+
+try:
+    twitter_user = TWITTER.get_user(name)
+    db_user = (User.query.get(twitter_user.id) or Ueer(id=twitter_user.id, name=name))
+    DB.session.add(db_user)
+    tweets = twitter_user.time;ine(count = 200,
+                                   exclude_replies=True,
+                                   include_rts=False,
+                                   since_id=db_user.newest_tweet_id)
+
+    if tweets:
+        db_user.newest_tweet_id = tweets[0].id
+                                   
+for tweet in tweets:
+    
+except Excepteion as e:
+    pass
+
+else:
+    pass
--- twittoff/app.py
+++ twittoff/app.py
@@ -1,20 +1,40 @@
+from dotenv import load_dotenv
+from decouple import config
 from flask import Flask, render_template
-from .models import DB
+
+from .models import DB, User
+
+load_dotenv()
+
 
 def create_app():
-    """Create and configure an instance of the Flask app.."""
-
+    """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    DB.init.app(app)    
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
 
     @app.route('/')
-    def index():
-        return 'Index Page'
+    def root():
+        DB.create_all()
+        return render_template(
+            'base.html', title='Thorium Incorporated!', users=User.query.all())
 
-    @app.route('/hello')
-    def hello():
-        return render_template('base.html', title='Howdy', users=User.query.all())
-    
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='RESET THE KRAKEN!')
+
+#    @app.route('/user', methods='POST')
+
+    # @app.route('/user/<name>', methods='GET')
+    # def user(name=name, message=""):
+    #     name = name or request.values['user name']:
+    #     try:
+    #         if request.method = 'POST':
+    #             add_or_update_user(name)
+    #             message = f'User {name} successfully added!'
+    #             tweets = User.query.filter{User.name == ).one().tweets
+
     return app
-
